07423eff5628d96ce7957d4f8c4d2823695a5453
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-12-04 19:27:07 -0800

    met mvp

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require("./server.js");
+
+server.listen(6700, () => {
+  console.log("server running on http://localhost:6700");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,149 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const Users = require("../users/userDb");
+const Posts = require("../posts/postDb");
+
+router.post("/", validateUser, (req, res) => {
   // do your magic!
+  Users.insert({ name: req.body.name })
+    .then(make => {
+      res.status(200).json(make);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error posting"
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validatePost, validateUserId, (req, res) => {
   // do your magic!
+  Posts.insert({
+    user_id: req.params.id,
+    text: req.body.text
+  })
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "error adding users post" });
+    });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  Users.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "error getting users" });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
+  Users.getUserPosts(req.params.id)
+    .then(byPost => {
+      res.status(200).json(byPost);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error getting posts"
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+    .then(async byeUser => {
+      await Users.remove(req.params.id);
+      res.status(200).json(byeUser);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing user"
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validateUserId, (req, res) => {
   // do your magic!
+  Users.update(req.params.id, {
+    name: req.body.name
+  })
+    .then(update => {
+      res.status(200).json(update);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error editing user"
+      });
+    });
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // do your magic!
+
+  try {
+    const user = await Users.getById(req.params.id);
+    req.user = user;
+
+    next();
+  } catch (err) {
+    res.status(400).json({
+      message: "invalid user id"
+    });
+  }
 }
 
-function validateUser(req, res, next) {
+async function validateUser(req, res, next) {
   // do your magic!
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({
+      message: "missing user data"
+    });
+  } else if (req.body.name && req.body.name.length) {
+    next();
+  } else {
+    res.status(400).json({
+      message: "missing required name field"
+    });
+  }
 }
 
-function validatePost(req, res, next) {
+async function validatePost(req, res, next) {
   // do your magic!
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({
+      message: "missing post data"
+    });
+  } else if (req.body.text && req.body.text.length) {
+    next();
+  } else {
+    res.status(400).json({
+      message: "missing required text field"
+    });
+  }
 }
 
 module.exports = router;
